f8b1f9f3adb1cfd17e7f331278d2d128ed36ed15
MartinKasapov <martin.kasapov.mk@gmail.com>
2019-06-03 19:05:57 +0300

    final commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,81 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import objectmodels.Employee;
+import objectmodels.ReportDefinition;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Application {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> jsonPaths = new ArrayList<>();
+        String line;
+        for (int i = 0; i <= 1; i++) {
+            line = reader.readLine();
+            jsonPaths.add(line);
+        }
+
+        String employeeJsonPath = jsonPaths.get(0);
+        String reportDefinitionJsonPath = jsonPaths.get(1);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<Employee> employees = mapper.readValue(new File(employeeJsonPath), new TypeReference<List<Employee>>() {
+        });
+        final ReportDefinition reportDefinition = mapper
+                .readValue(new File(reportDefinitionJsonPath), new TypeReference<ReportDefinition>() {
+                });
+
+        List<Employee> employeesWithValidSalesPeriod = getEmployeesWithValidSalesPeriod(employees, reportDefinition);
+
+        List<Double> filteredScores = getValidScores(reportDefinition, employeesWithValidSalesPeriod);
+
+        writeCsvResultFile(employeesWithValidSalesPeriod, filteredScores);
+
+
+    }
+
+    private static List<Employee> getEmployeesWithValidSalesPeriod(List<Employee> employeeObjects, ReportDefinition reportDefinition) {
+        return employeeObjects.stream()
+                .filter(emp -> emp.getSalesPeriod() <= reportDefinition.getPeriodLimit())
+                .collect(Collectors.toList());
+    }
+
+    private static void writeCsvResultFile(List<Employee> employeesWithGoodSalesPeriod, List<Double> filteredScores) throws IOException {
+        FileWriter csvWriter = new FileWriter("result.csv");
+        csvWriter.append("Name");
+        csvWriter.append(",");
+        csvWriter.append("Score");
+        csvWriter.append("\n");
+
+        for (Employee employee : employeesWithGoodSalesPeriod) {
+            if (filteredScores.contains(employee.getScore())) {
+                csvWriter.append(employee.getName() + ", ");
+                csvWriter.append(employee.getScore() + "");
+                csvWriter.append("\n");
+            }
+        }
+        csvWriter.flush();
+        csvWriter.close();
+    }
+
+    private static List<Double> getValidScores(ReportDefinition reportDefinition, List<Employee> employeesWithGoodSalesPeriod) {
+        double score = 0;
+        List<Double> scores = new ArrayList<>();
+        for (Employee employee : employeesWithGoodSalesPeriod) {
+            if (reportDefinition.isUseExperienceMultiplier()) {
+                score = (employee.getTotalSales() / employee.getSalesPeriod()) * employee.getExperienceMultiplier();
+            } else {
+                score = employee.getTotalSales() / employee.getSalesPeriod();
+            }
+            scores.add(score);
+            employee.setScore(score);
+        }
+        Collections.sort(scores);
+        int numberOfTopScores = scores.size() * reportDefinition.getTopPerformersThreshold() / 100;
+
+        return scores.stream().limit(numberOfTopScores).collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/objectmodels/Employee.java
@@ -0,0 +1,60 @@
+package objectmodels;
+
+public class Employee {
+    private String name;
+    private int totalSales;
+    private int salesPeriod;
+    private double experienceMultiplier;
+    private double score;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int totalSales, int salesPeriod, double experienceMultiplier) {
+        this.name = name;
+        this.totalSales = totalSales;
+        this.salesPeriod = salesPeriod;
+        this.experienceMultiplier = experienceMultiplier;
+        this.score=0.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(int totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public int getSalesPeriod() {
+        return salesPeriod;
+    }
+
+    public void setSalesPeriod(int salesPeriod) {
+        this.salesPeriod = salesPeriod;
+    }
+
+    public double getExperienceMultiplier() {
+        return experienceMultiplier;
+    }
+
+    public void setExperienceMultiplier(double experienceMultiplier) {
+        this.experienceMultiplier = experienceMultiplier;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
